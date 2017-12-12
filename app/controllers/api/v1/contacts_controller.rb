067622c2869dff57283cb48d90ad9602850178aa
Victor Pragana <victormerigue@gmail.com>
2017-12-12 21:34:03 -0200

    Update contacts_controller.rb

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ContactsController < ApplicationController
-    class Api::V1::ContactsController < Api::V1::ApiController
 
 before_action :set_contact, only: [:show, :update, :destroy]
 
